7bed10fcb5f33b1dd6d19d5b2aa8a87665a72cec
bheney <benjamin.heney@unh.edu>
2022-12-03 23:53:00 -0500

    Added enable_pipe method. Calling it for the night.

--- radio.py
+++ radio.py
@@ -1,5 +1,6 @@
 class Radio:
-    def __init__(self, mosi, miso, clk, cs, ce):
+    def __init__(self, mosi, miso, clk, cs, ce, address_width=5, frequency=2,
+                 air_data_rate=1, lna_gain=):
         """
         Initialize an instance of an nRF24l01 transceiver
         :param mosi: int, MOSI pin
@@ -11,6 +12,7 @@ class Radio:
         self.spi = pispi.Spi(mosi, miso, clk, cs, True)
         self.spi.bit_rate = 10000
         self.ce = ce
+        self.pipe={'0':0xE7E7E7E7E7, '1':0xC2C2C2C2C2}
         GPIO.setup(self.ce, GPIO.OUT)
     def write(self, register,*data):
         """
@@ -46,6 +48,64 @@ class Radio:
         """
         address = address & 0b00011111
         return self.spi.transaction(address,1)
+    def enable_pipe(self, pipe_id, pipe_address, auto_ack=True, dynamic=True,
+                    payload_len=32):
+        # Enable Pipe
+        mask=(254+1)-(2^pipe_id)
+        bit=1<<pipe_id
+        self.write_mask(0x02,mask,bit)
+
+        # Enable/Disable AutoAck
+        if auto_ack:
+            self.write_mask(0x01,mask,bit)
+        else:
+            self.write_mask(0x01,mask,~bit)
+
+        # Set payload width
+        if dynamic:
+            self.write_mask(0x1D,0b11111011,0b100)
+            self.write_mask(0x1C,mask,bit)
+            if not auto_ack:
+                raise Exception
+        else:
+            register=0x11+pipe_id
+            self.write(register,payload_len)
+
+        # Set address
+        address_width = self.get(0x03) + 2
+        msb = pipe_address.to_bytes(address_width, 'big')
+        lsb = pipe_address.to_bytes(address_width, 'little')
+        if pipe_id !=0 and pipe_address==self.pipe['0']:
+            raise Exception('Address cannot be the same as Pipe 0')
+
+        if pipe_id=1:
+            bit_address=''
+            for byte in msb:
+                bit_address+=bin(int(byte))[2:]
+            if bit_address[0:1] == '10':
+                raise Exception('Address cannot start with 0b10')
+
+            state = bit_address[0]
+            shift = 0
+            for bit in bit_address:
+                if bit != state:
+                    shift += 1
+                state = bit
+            if shift <= 1:
+                raise Exception(
+                    'address must contain more than one level shift')
+
+        for check_pipe in self.pipe:
+            address=self.pipe[check_pipe]
+            check_address_bytes=address.to_bytes(address_width,'big')
+            if check_pipe != pipe_id:
+                if check_address_bytes[-1] == msb[-1]:
+                    raise Exception("LSB of pipe address must be unique")
+
+        self.write(0x0A+pipe_id,)
+
+
+
 
     def listen(self):
         self.write_mask(0x00,0b11111110,0b1)
